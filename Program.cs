3723cb80f51060bd950c470bb702bdbcfde91f39
Abdulkadirbulbul <kadirwinchester25@gmail.com>
2021-03-15 22:17:01 +0300

    vb1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           Console.WriteLine("x Factorial = Y, please enter x?");  
+            int a = Convert.ToInt32(Console.ReadLine());  
+            int fakt(int n)        //creating a new recursion methodology
+            {
+                if (n == 1)
+                {
+                    return 1;             
+                }
+                else return n * fakt(n - 1);    //The result of multiplying by 
+                                                //the previous number is always returned    
+            }
+            Console.WriteLine("Result= " + fakt(a));    //Print the result 
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,20 @@
+
+<html>
+
+<body>
+	<h3> Recursion Factorial Method; </h3>
+<hr>
+<p>
+<br> 
+Bu repomuzda Recursion  metodunun kullanarak faktöriyel işlem nasul yapılır bunu gösterdim. <br>
+
+
+In this repository, I showed how to do factorial operation using Recursion method. <br>
+
+Video link coming to my channel soon...
+</p>
+<hr>
+
+
+</body>
+</html>
